9fce64219498277f2b580c1e092cfc620725004d
Apoorv Purohit <apoorv_prht@yahoo.com>
2014-12-21 20:10:15 +0000

    Pushing latest code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "XR4F8t3fcqWC1Xh7hrnYFnPmk", "yL6ov3kPXRUfN6WppII3pELb5e97JNh4vASYEXOkEAraLPiPM6"
+  provider :twitter, "6u5Y0WI1zxvZ6t8Z6ovotwM73", "XVIoRuDEIHvHnr5dSANHcu1O8fUBseYmrrvNzrFfA3nvzW9gxe"
 end
